304e3cc2c19e1260524f1bf0c2454d1146fe13b6
Robert J. Mitchell <robert.judson@outlook.com>
2020-01-17 01:05:55 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
-function App() {
+const App = () => {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
