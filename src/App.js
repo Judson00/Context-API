b7ea0973da5c0b2988f6cc7e2df900c35c47e6ad
Robert J. Mitchell <robert.judson@outlook.com>
2020-01-20 19:28:07 -0500

    followed directions in README.md, finished MVP

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import data from './data';
 
 // Components
@@ -7,34 +7,44 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+//Context
+import { ProductContext } from './contexts/ProductContext';
+import { CartContext } from './contexts/CartContext';
+
 const App = () => {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
-		// add the given item to the cart
+		const newItem = {
+			id: item.id,
+			title: item.title,
+			price: item.price,
+			image: item.image
+		}
+
+		setCart([...cart, newItem])
 	};
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
+			<Switch>
+				<ProductContext.Provider value={{ products, addItem }}>
+					<CartContext.Provider value={{ cart }}>
+						<Navigation />
+							
+							{/* Routes */}
+							<Route exact path="/">
+								<Products />
+							</Route>
 
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
-
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
+							<Route path="/cart">
+								<ShoppingCart />
+							</Route>
+						
+					</CartContext.Provider>
+				</ProductContext.Provider>
+			</Switch>
 		</div>
 	);
 }
